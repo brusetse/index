c842bce81ae8113f06d0a32159908a8f30ecf31c
xbc <xbc@ivyb2b.com>
2019-09-19 14:34:07 +0800

    add gateway

---
+++ spring/cloud/netflix/gateway/src/main/java/com/bruse/gateway/GatewayConfig.java
@@ -0,0 +1,17 @@
+package com.bruse.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("gitee_route", r -> r.path("/gitee").uri("http://39.108.174.27:3000"))
+                .build();
+    }
+}
